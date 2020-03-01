7064ac7d9db362761d1b871f761ae0f1281d75ea
gamerakshay <48151925+gamerakshay@users.noreply.github.com>
2020-03-02 00:46:40 +0530

    Update index.js

--- daaru/index.js
+++ daaru/index.js
@@ -1,51 +1,40 @@
 var n1=0;
 $(".question1 input").on("click",function(){
-
   if($(".question1 input")[0].checked)
   {
-
     n1+=2;
-    console.log(n1);
   }
   else if($(".question1 input")[1].checked)
   {
-
     n1+=5;
-    console.log(n1);
   }
   else {
-
     n1+=10;
-    console.log(n1);
   }
-    $(".question2").show("slow","swing");
-$(".question1").hide("slow","swing");
-
+  $(".question2").show("slow","swing");
+  $(".question1").hide("slow","swing");
  // $(".question1").addClass("displayof");
-
-
-
 });
 
-console.log(n1);
+
 
 $(".question2 input").on("click",function(){
   if($(".question2 input")[0].checked)
   {
 
     n1+=2;
-    console.log(n1);
+
   }
   else if($(".question2 input")[1].checked)
   {
 
     n1+=5;
-    console.log(n1);
+
   }
   else {
 
     n1+=10;
-    console.log(n1);
+
   }
   $(".question3").show("slow","swing");
 $(".question2").hide("slow","swing");
@@ -60,18 +49,18 @@ $(".question3 input").on("click",function(){
   {
 
     n1+=2;
-    console.log(n1);
+
   }
   else if($(".question3 input")[1].checked)
   {
 
     n1+=5;
-    console.log(n1);
+
   }
   else {
 
     n1+=10;
-    console.log(n1);
+
   }
   $(".question4").show("slow","swing");
 
@@ -86,18 +75,18 @@ $(".question4 input").on("click",function(){
   {
 
     n1+=2;
-    console.log(n1);
+
   }
   else if($(".question4 input")[1].checked)
   {
 
     n1+=5;
-    console.log(n1);
+
   }
   else {
 
     n1+=10;
-    console.log(n1);
+
   }
   $(".question5").show("slow","swing");
 
@@ -113,18 +102,18 @@ $(".question5 input").on("click",function(){
   {
 
     n1+=2;
-    console.log(n1);
+
   }
   else if($(".question5 input")[1].checked)
   {
 
     n1+=5;
-    console.log(n1);
+
   }
   else {
 
     n1+=10;
-    console.log(n1);
+
   }
     $(".question6").show("slow","swing");
 $(".question5").hide("slow","swing");
@@ -136,19 +125,20 @@ $(".question6 input").on("click",function(){
   {
 
     n1+=2;
-    console.log(n1);
+
   }
   else if($(".question6 input")[1].checked)
   {
 
     n1+=5;
-    console.log(n1);
+
   }
   else {
 
     n1+=10;
-    console.log(n1);
+
   }
+    result(n1);
   $(".Output h1").html(n1);
   $(".mainbody").addClass("displayof");
   $(".Output").removeClass("displayof");
@@ -156,12 +146,14 @@ $(".question6 input").on("click",function(){
 
 $(".question6").hide("slow","swing");
       $(".question6").fadeOut(slow);
+
 });
 
 
 $(".Output1").on("click",function(){
     $(".Output").addClass("displayof");
       $(".Output1").addClass("displayof");
+
 n1=0;
        $(".mainbody").removeClass("displayof");
       $(".question1").show("slow","swing");
@@ -169,3 +161,32 @@ n1=0;
 
 
 });
+function result(n1)
+{
+  if(n1>=12&&n1<=20)
+  {
+
+    document.querySelector(".Output img").setAttribute("src","assets/fruit-beer.jpg");
+  }
+  else if(n1>=20&&n1<=28)
+  {
+document.querySelector(".Output img").setAttribute("src","assets/Beer.jpg");
+  }
+else  if(n1>=28&&n1<=36)
+  {
+document.querySelector(".Output img").setAttribute("src","assets/Whisky.jpg");
+  }
+else  if(n1>=36&&n1<=44)
+  {
+document.querySelector(".Output img").setAttribute("src","assets/vodka.jpeg");
+  }
+else   if(n1>=44&&n1<=52)
+  {
+document.querySelector(".Output img").setAttribute("src","assets/Tequilla.jpg");
+  }
+  else if(n1>=52&&n1<=60)
+  {
+document.querySelector(".Output img").setAttribute("src","assets/Rum.jpg");
+  }
+
+}
